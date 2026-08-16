0542acf60b598055364d138667bb55b8fe14b6e1
Jack Curtis <jackcurtis121@gmail.com>
2026-08-16 13:08:08 -0400

    Suppress spurious spotbugs

--- src/main/java/jc121f1/wbs/handlers/InstanceHandler.java
+++ src/main/java/jc121f1/wbs/handlers/InstanceHandler.java
@@ -1,9 +1,13 @@
 package jc121f1.wbs.handlers;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jc121f1.services.instance.InstanceService;
 
 public abstract class InstanceHandler implements BaseHandler {
-
+    @SuppressFBWarnings(
+            value = "EI_EXPOSE_REP2",
+            justification = "InstanceService is an injected service dependency and is intentionally shared."
+    )
     protected final InstanceService instanceService;
 
     public InstanceHandler(InstanceService instanceService) {
