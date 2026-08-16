2b269256f496a011ed8eecb5623ea3e5e43c138e
Jack Curtis <jackcurtis121@gmail.com>
2026-08-16 01:53:37 -0400

    Implement list api

--- src/main/java/jc121f1/wbs/handlers/get/ListInstanceHandler.java
+++ src/main/java/jc121f1/wbs/handlers/get/ListInstanceHandler.java
@@ -1,11 +1,13 @@
 package jc121f1.wbs.handlers.get;
 
 import io.javalin.http.Context;
+import jc121f1.model.instance.dao.Instance;
 import jc121f1.services.instance.InstanceService;
 import jc121f1.wbs.handlers.InstanceHandler;
 import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class ListInstanceHandler extends InstanceHandler {
 
@@ -16,6 +18,8 @@ public class ListInstanceHandler extends InstanceHandler {
 
     @Override
     public void handle(@NotNull Context ctx) throws Exception {
+        List<Instance> instances = instanceService.list();
 
+        ctx.json(instances);
     }
 }
