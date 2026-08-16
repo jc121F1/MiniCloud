65922a7c1e64efb275c15bb4dbf2e0d604a2dd5c
Jack Curtis <jackcurtis121@gmail.com>
2026-08-16 01:28:36 -0400

    Add partial InstanceService interface

--- src/main/java/jc121f1/services/instance/InstanceService.java
+++ src/main/java/jc121f1/services/instance/InstanceService.java
@@ -1,10 +1,18 @@
 package jc121f1.services.instance;
 
+import jc121f1.model.instance.api.CreateInstanceRequest;
 import jc121f1.model.instance.api.GetInstanceRequest;
+import jc121f1.model.instance.api.ListInstanceRequest;
 import jc121f1.model.instance.dao.Instance;
 
-public class InstanceService {
-    public Instance get(GetInstanceRequest request) {
-        return null;
-    }
+import java.util.List;
+
+public interface InstanceService {
+    Instance get(GetInstanceRequest request);
+
+    Instance create(CreateInstanceRequest request);
+
+    List<Instance> list(ListInstanceRequest request);
+
+    List<Instance> list();
 }
