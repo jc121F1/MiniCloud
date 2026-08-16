475c22c003c7f3aec4fe90020afbbb9b8d1bf595
Jack Curtis <jackcurtis121@gmail.com>
2026-08-16 01:53:28 -0400

    Add serialization for createdAt instant

--- src/main/java/jc121f1/model/instance/dao/Instance.java
+++ src/main/java/jc121f1/model/instance/dao/Instance.java
@@ -1,11 +1,14 @@
 package jc121f1.model.instance.dao;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jc121f1.model.instance.InstanceState;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Getter
 @Builder
@@ -21,5 +24,11 @@ public class Instance {
     @Setter
     private InstanceState state;
 
+    @JsonIgnore
     private Instant createdAt;
+
+    @JsonProperty("createdAt")
+    public String createdAtAsString() {
+        return createdAt.truncatedTo(ChronoUnit.SECONDS).toString();
+    }
 }
