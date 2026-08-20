8566e29325449c732a20ab60f4ad3a13245b4a0d
Jack Curtis <jackcurtis121@gmail.com>
2026-08-19 22:47:44 -0400

    Implement DeleteInstanceHandler
    Signed-off-by: Jack Curtis <jackcurtis121@gmail.com>

--- src/main/java/jc121f1/wbs/handlers/delete/DeleteInstanceHandler.java
+++ src/main/java/jc121f1/wbs/handlers/delete/DeleteInstanceHandler.java
@@ -1,6 +1,8 @@
 package jc121f1.wbs.handlers.delete;
 
 import io.javalin.http.Context;
+import jc121f1.model.instance.api.request.DeleteInstanceRequest;
+import jc121f1.model.instance.dao.Instance;
 import jc121f1.services.instance.InstanceService;
 import jc121f1.wbs.handlers.InstanceHandler;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +18,10 @@ public class DeleteInstanceHandler extends InstanceHandler {
 
     @Override
     public void handle(@NotNull Context ctx) throws Exception {
+        DeleteInstanceRequest request = ctx.bodyAsClass(DeleteInstanceRequest.class);
 
+        Instance instance = instanceService.delete(request);
+
+        ctx.json(instance);
     }
 }
