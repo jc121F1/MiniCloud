c9fd42f3c43f46d425d4cf3675dbc95336fbd86e
Jack Curtis <jackcurtis121@gmail.com>
2026-08-19 22:49:31 -0400

    Allow for env variable or property to be used whe passing docker env variables
    Signed-off-by: Jack Curtis <jackcurtis121@gmail.com>

--- src/main/java/jc121f1/dagger/EnvironmentModule.java
+++ src/main/java/jc121f1/dagger/EnvironmentModule.java
@@ -7,29 +7,31 @@ import jc121f1.dagger.qualifiers.RegistryPass;
 import jc121f1.dagger.qualifiers.RegistryUrl;
 import jc121f1.dagger.qualifiers.RegistryUser;
 
+import java.util.Optional;
+
 @Module
 public class EnvironmentModule {
     @Provides
     @RegistryUser
     String providerRegistryUser() {
-        return System.getenv("DOCKER_USER");
+        return Optional.ofNullable(System.getenv("DOCKER_USER")).orElse(System.getProperty("DOCKER_HOST"));
     }
 
     @Provides
     @RegistryPass
     String providerRegistryPass() {
-        return System.getenv("DOCKER_PASS");
+        return Optional.ofNullable(System.getenv("DOCKER_PASS")).orElse(System.getProperty("DOCKER_PASS"));
     }
 
     @Provides
     @RegistryUrl
     String providerRegistryUrl() {
-        return System.getenv("DOCKER_URL");
+        return Optional.ofNullable(System.getenv("DOCKER_URL")).orElse(System.getProperty("DOCKER_URL"));
     }
 
     @Provides
     @RegistryMail
     String providerRegistryMail() {
-        return System.getenv("DOCKER_MAIL");
+        return Optional.ofNullable(System.getenv("DOCKER_MAIL")).orElse(System.getProperty("DOCKER_MAIL"));
     }
 }
