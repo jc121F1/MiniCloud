8211b621d793317bfa119fb7b4e1ead18a8990ab
Jack Curtis <jackcurtis121@gmail.com>
2026-08-20 23:09:01 -0400

    Add qualifiers that were not pushed as part of 937809d46fccf1a7e5d831ae935f8926f9ca6492
    Signed-off-by: Jack Curtis <jackcurtis121@gmail.com>

---
+++ src/main/java/jc121f1/dagger/qualifiers/Debug.java
@@ -0,0 +1,12 @@
+package jc121f1.dagger.qualifiers;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Debug {
+}
