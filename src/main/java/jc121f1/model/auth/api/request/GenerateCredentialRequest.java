31709dff4632b6c508680d66edcdfb1f3ab6c1c6
Jack Curtis <jackcurtis121@gmail.com>
2026-09-05 21:41:37 -0400

    Implement Credential requests
    Signed-off-by: Jack Curtis <jackcurtis121@gmail.com>

---
+++ src/main/java/jc121f1/model/auth/api/request/GenerateCredentialRequest.java
@@ -0,0 +1,8 @@
+package jc121f1.model.auth.api.request;
+
+import io.javalin.openapi.OpenApiRequired;
+import lombok.Builder;
+
+@Builder
+public record GenerateCredentialRequest(@OpenApiRequired String email, @OpenApiRequired String password) {
+}
