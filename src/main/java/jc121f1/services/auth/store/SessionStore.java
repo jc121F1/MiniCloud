669bb5261e4f03306bdb38e7a12065367ae2dcbf
Jack Curtis <jackcurtis121@gmail.com>
2026-09-05 21:39:40 -0400

    Implement SessionStore
    Signed-off-by: Jack Curtis <jackcurtis121@gmail.com>

---
+++ src/main/java/jc121f1/services/auth/store/SessionStore.java
@@ -0,0 +1,7 @@
+package jc121f1.services.auth.store;
+
+import jc121f1.common.store.GenericStore;
+import jc121f1.model.auth.dao.Session;
+
+public interface SessionStore extends GenericStore<Session> {
+}
