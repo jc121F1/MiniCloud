7dad10f93d7b176804673a900563e860078a5fe6
Jack Curtis <jackcurtis121@gmail.com>
2026-09-05 21:37:02 -0400

    Add UnauthorizedException
    Signed-off-by: Jack Curtis <jackcurtis121@gmail.com>

---
+++ src/main/java/jc121f1/services/instance/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package jc121f1.services.instance.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
