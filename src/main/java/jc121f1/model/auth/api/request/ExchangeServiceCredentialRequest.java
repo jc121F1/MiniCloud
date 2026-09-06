b445fdbaef45113f6029a5218fbce7967ea87d2a
Jack Curtis <jackcurtis121@gmail.com>
2026-09-05 21:41:37 -0400

    Implement Credential requests
    Signed-off-by: Jack Curtis <jackcurtis121@gmail.com>

---
+++ src/main/java/jc121f1/model/auth/api/request/ExchangeServiceCredentialRequest.java
@@ -0,0 +1,4 @@
+package jc121f1.model.auth.api.request;
+
+public record ExchangeServiceCredentialRequest(String credentialId, String secret) {
+}
---
+++ src/main/java/jc121f1/model/auth/api/request/InvalidateCredentialRequest.java
@@ -0,0 +1,4 @@
+package jc121f1.model.auth.api.request;
+
+public record InvalidateCredentialRequest(String credentialId) {
+}
